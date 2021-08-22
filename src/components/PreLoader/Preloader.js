58aaf26d27217c0458d431db04626e9930805401
Bulat <bulatkazan@yahoo.com>
2021-08-22 17:50:32 +0300

    test commit

--- src/components/PreLoader/Preloader.js
+++ src/components/PreLoader/Preloader.js
@@ -1,7 +1,7 @@
 import preloader from '../../assets/preloader.svg'
 
 const Preloader = () => {
-  return <div><img src={preloader} alt="Loading..."/></div>
+  return <div><img src={preloader} alt="Loading"/></div>
 }
 
 export default Preloader
